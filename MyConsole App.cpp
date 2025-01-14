3cc745ab1950650ba7ddc780720fa12679a0039a
Brenna Meyer <brennameyer9@gmail.com>
2025-01-14 13:49:24 -0500

    Add project files.

---
+++ MyConsole App.cpp
@@ -0,0 +1,16 @@
+/*
+File Name:MyConSole App.cpp
+Programmer:Brenna Meyer
+Date:January 2025
+Requirements:
+Add two number together ( 50 and 100 ) display result.
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
