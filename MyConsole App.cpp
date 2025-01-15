13723f81eacb0fb15e006042e02983ea1273382c
Brenna Meyer <brennameyer9@gmail.com>
2025-01-15 13:05:09 -0500

    Added code to accept and display first name.

--- MyConsole App.cpp
+++ MyConsole App.cpp
@@ -10,7 +10,20 @@ Add two number together ( 50 and 100 ) display result.
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	char firstInitial;
+	char middleInitial;
+	char lastInitial;
+	std::string firstName;
+	std::cout << "Please enter the initial of your first name:";
+	std::cin >> firstInitial;
+	std::cout << "Please enter the initial of your middle name:";
+	std::cin >> middleInitial;
+	std::cout << "Please enter the initial of your last name:";
+	std::cin >> lastInitial;
+	std::cout << "Your initals are: " << firstInitial << middleInitial << lastInitial << std::endl;
+	std::cout << "Please enter your first name:";
+	std::cin >> firstName;
+	std::cout << "Your first name is : " << firstName;
 }
 
 
